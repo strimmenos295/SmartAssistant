15b8bcad93d25ce695d01e40f992e921abf91a44
Dimitris Lamporu <p19086@unipi.gr>
2022-02-21 20:29:02 +0200

    21/2/2022 8:30

--- Form7.Designer.cs
+++ Form7.Designer.cs
@@ -33,14 +33,16 @@ namespace SmartAssistant
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(80, 94);
+            this.richTextBox1.Location = new System.Drawing.Point(120, 145);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.ReadOnly = true;
-            this.richTextBox1.Size = new System.Drawing.Size(464, 245);
+            this.richTextBox1.Size = new System.Drawing.Size(694, 375);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             // 
@@ -48,17 +50,19 @@ namespace SmartAssistant
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(182, 35);
+            this.label1.Location = new System.Drawing.Point(273, 54);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(256, 24);
+            this.label1.Size = new System.Drawing.Size(369, 33);
             this.label1.TabIndex = 1;
             this.label1.Text = "Πλάνο σημερινής ημέρας";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(639, 94);
+            this.button1.Location = new System.Drawing.Point(958, 145);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(110, 39);
+            this.button1.Size = new System.Drawing.Size(165, 60);
             this.button1.TabIndex = 2;
             this.button1.Text = "Εισαγωγή Δραστηριότητας";
             this.button1.UseVisualStyleBackColor = true;
@@ -66,23 +70,39 @@ namespace SmartAssistant
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(639, 139);
+            this.button2.Location = new System.Drawing.Point(958, 214);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(110, 37);
+            this.button2.Size = new System.Drawing.Size(165, 57);
             this.button2.TabIndex = 3;
             this.button2.Text = "Διαγραφή Δραστηριότητας";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.BackColor = System.Drawing.Color.Crimson;
+            this.button4.Font = new System.Drawing.Font("Century", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.button4.Location = new System.Drawing.Point(958, 15);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(192, 114);
+            this.button4.TabIndex = 53;
+            this.button4.Text = "Αρχική Σελίδα";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form7
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(901, 450);
+            this.ClientSize = new System.Drawing.Size(1352, 692);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.richTextBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form7";
             this.Text = "Form7";
             this.Load += new System.EventHandler(this.Form7_Load);
@@ -97,5 +117,6 @@ namespace SmartAssistant
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
     }
 }
